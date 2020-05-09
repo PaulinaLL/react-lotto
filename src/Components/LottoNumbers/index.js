abc53d781f8f6d3c39109c74428e8b9c3fbe6418
PaulinaLL <paulinalll90@wp.pl>
2020-05-09 22:45:47 +0200

    Add logic to the LuckyNumbers Component

--- src/Components/LottoNumbers/index.js
+++ src/Components/LottoNumbers/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getRandomNum, getRandomNumbers } from "../../helpers";
 
 class LuckyNumbers extends React.Component {
   constructor(props) {
@@ -6,22 +7,44 @@ class LuckyNumbers extends React.Component {
 
     this.state = {
       numbers: [],
-      number: null,
+      luckyNumber: null,
     };
 
     this.handleClick = this.handleClick.bind(this);
     this.resetClick = this.resetClick.bind(this);
   }
-  handleClick() {}
 
-  resetClick() {}
+  handleClick() {
+    this.setState({
+      numbers: getRandomNumbers(6),
+      luckyNumber: getRandomNum(1, 11),
+    });
+  }
+
+  resetClick() {
+    this.setState({
+      numbers: [],
+      luckyNumber: null,
+    });
+  }
 
   render() {
     return (
       <>
-        <div className="numbers"></div>
-        <button>Show me lucky numbers:</button>
-        <button>Reset</button>
+        <div className="numbers">
+          {this.state.numbers.map((num, index) => {
+            return (
+              <div className="number balls" key={index}>
+                {num}
+              </div>
+            );
+          })}
+          <div>{this.state.luckyNumber}</div>
+        </div>
+        <div className="buttons">
+          <button onClick={this.handleClick}>Show me lucky numbers:</button>
+          <button onClick={this.resetClick}>Reset</button>
+        </div>
       </>
     );
   }
