f428f0294e6c5ffb17fe3c1097f3d6f015176083
PaulinaLL <paulinalll90@wp.pl>
2020-05-09 22:44:52 +0200

    Add a headline and Lucky Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import LuckyNumbers from "./Components/LottoNumbers";
 
 function App() {
   return (
     <div className="App">
-      <main className="App-main"></main>
+      <main className="App-main">
+        <h2>Get the lucky numbers!</h2>
+        <LuckyNumbers />
+      </main>
     </div>
   );
 }
